0aa8a38c755ed5d2e0cff2f820d4911271166feb
阿良良 ALiangLiang <me@aliangliang.top>
2018-03-15 15:28:23 +0800

    break coding style
    for test

--- api/order.js
+++ api/order.js
@@ -4,7 +4,7 @@ const CryptoJS = require('crypto-js')
 require('./../lib/jCryption.js')
 const $ = global.$
 
-const order = async function (opt) {
+const order = async function(opt) {
   console.log('test')
   try {
     const res = await request({
@@ -34,25 +34,25 @@ const order = async function (opt) {
       'RecTel': '', // 收貨人連絡電話 - 市話。
       'RecMobile': opt.RecMobile, // 收貨人連絡電話 - 手機。
       'RecZip': opt.RecZip, // 收貨人郵遞區號。
-      'RecAddress': opt.RecAddress, // 收貨人地址。
+       'RecAddress': opt.RecAddress, // 收貨人地址。
       'AddContact': 'N', // 資料加入收貨人通訊錄。
-      'ConfirmIsLand': 'N',
+       'ConfirmIsLand': 'N',
       'RecMail': '',
-      'PaperInvoice': 'N', // 是否願意將發票進行捐贈。
+       'PaperInvoice': 'N', // 是否願意將發票進行捐贈。
       'InvoiceType': 'P', // 個人電子發票。
       'TaxNO': '', // 發票種類為公司戶電子發票時，統一編號。
-      'AddTaxNO': 'N', // 資料加入公司統編備忘錄。
+          'AddTaxNO': 'N', // 資料加入公司統編備忘錄。
       'CashPoint': '0',
-      'Token': '',
+       'Token': '',
       'DeviceID': '',
       'DeviceOS': '',
-      'DeviceName': '',
+          'DeviceName': '',
       'DeviceOSVersion': '',
       'DeviceAppVersion': '',
-      'IsSkipOTP': 'N',
+       'IsSkipOTP': 'N',
       'availableDepositPoint': '0',
       'availableVoucherPoint': '0',
-      'depositUsed': '0',
+         'depositUsed': '0',
       'voucherUsed': '0',
       'BindMobile': ''
     }
@@ -91,7 +91,7 @@ const order = async function (opt) {
 
     return JSON.parse(result)
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
 
