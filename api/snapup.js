78cfa963435cabceca1e05388b3988a7b02b40c0
ALiangLiang <me@aliangliang.top>
2020-05-24 11:01:01 +0000

    Update test env

--- api/snapup.js
+++ api/snapup.js
@@ -1,13 +1,10 @@
 const snapup = async function (productionId) {
-  console.log(productionId)
   const res = await this._request({
     url: `https://24h.pchome.com.tw/prod/cart/v1/prod/${productionId}/snapup?_callback=jsonp_cartsnapup&_=${new Date().getTime()}`,
     method: 'get',
     jar: this._jar
   })
   const data = JSON.parse(res.match(/jsonp_cartsnapup\((.*?)\)/)[1])
-  console.log(`https://24h.pchome.com.tw/prod/cart/v1/prod/${productionId}/snapup?_callback=jsonp_cartsnapup&_=${new Date().getTime()}`)
-  console.log(data)
   // Status: 'OK', 'ProdLocked'
   if (data.Status !== 'OK' || !data.MACExpire || !data.MAC) {
     throw new Error('Cannot pass snapup API')
--- test.js
+++ test.js
@@ -4,18 +4,25 @@ const API = require('./api')
 const productionId = process.env.PROD_ID
 
 async function test () {
+  console.log('env:')
+  console.log('  PROD_ID:', process.env.PROD_ID)
+  console.log('  COOKIE_ECC:', process.env.COOKIE_ECC)
+  console.log('  COOKIE_COOKIE_ECWEBSESS:', process.env.COOKIE_COOKIE_ECWEBSESS)
+
   // 設定 cookie
   const api = new API({
     ECC: process.env.COOKIE_ECC,
-    COOKIE_ECWEBSESS: process.env.COOKIE_COOKIE_ECWEBSESS
+    ECWEBSESS: process.env.COOKIE_COOKIE_ECWEBSESS
   })
 
   // 在加入購物車前，必須先呼叫這支 API，來取得產品狀況
   const snapupResult = await api.snapup(productionId)
+  console.log('snapup result:', snapupResult)
 
   // 加入購物車
   const add2CartResult = await api.add2Cart(productionId, snapupResult, 1)
-  assert(add2CartResult.PRODCOUNT > 0)
+  console.log('add2Cart result:', add2CartResult)
+  assert(Number(add2CartResult.PRODCOUNT) > 0 || Number(add2CartResult.PRODADD) > 0)
 
   /**
    * 取得購物車資料與送出訂單，皆須登入，所以就不測試了
