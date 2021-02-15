3d52a88baf7a28aa1957c725c120358f1b516737
Wei-Liang Liou <me@aliangliang.top>
2021-02-16 02:54:20 +0800

    Update order.js

--- api/order.js
+++ api/order.js
@@ -14,7 +14,7 @@ const order = async function (opt) {
     $.jCryption.crypt.setKey(res.PK)
 
     const frmData = {
-      'PayWay': 'COD', // 付款方式，COD為貨到付款。
+      'PayWay': opt.payWay, // 付款方式
       'CusName': opt.cusName, // 購買人姓名。
       'CusMobile': opt.cusMobile, // 購買人連絡電話 - 手機。
       'AcceptEDM': 'N', // 是否願意收到PChome商品特惠通知。
